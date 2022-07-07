038bddd6d0ab370c0094980ca68a0fc7614dd9b1
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2022-07-07 13:43:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Ashik231/NGC_PRO_IMDB.git /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
+git clone -b master https://github.com/Ashik231/NGC_PRO_IMDB.git /NGC_PRO_IMDB
+cd /NGC_PRO_IMDB
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
